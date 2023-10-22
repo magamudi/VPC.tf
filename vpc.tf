eaef58a2be8742d67cef83af1f4f99cb64be426d
Mag. A <118616073+magamudi@users.noreply.github.com>
2023-10-22 14:01:48 -0500

    Update vpc.tf
    changed dns hostname to true again

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc-10-1" {
   assign_generated_ipv6_cidr_block = false
   cidr_block                       = "10.1.0.0/16"
-  enable_dns_hostnames             = false
+  enable_dns_hostnames             = true
   enable_dns_support               = true
   instance_tenancy                 = "default"
   tags = {
